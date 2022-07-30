cbbf4b4518229031c29301e178b4666fc86ae291
Dejitaru <dejitaru@gmail.com>
2022-07-30 17:37:19 -0500

    Ajustes para las confirmaciones

--- confirmacion.php
+++ confirmacion.php
@@ -1,3 +1,16 @@
+<?php
+//esta línea nos evita mostrar errores tipo warning
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try{
+    //$conexion=mysqli_connect('localhost','root','','susy');
+    $conexion=mysqli_connect('localhost','root','secret','susy');
+
+}catch(Exception $e) {
+    die('No hay conexión a la base de datos');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,43 +21,40 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
-/*
-$data['todook'] =  true;   
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($data);
-*/
-if(!isset($_POST['id'])) {
-  echo 'ERROR';
-  die();
-}
-$id = strip_tags($_POST['id']);  
-   //si estamos aqui, si trajo un ID
-   //actualizacion en mysql para confirmr los boletos de la persona
-    
+<?php
+ $mes = date('m');
+ $dia = date('d');
+ $hayErrores = false;
+ $mensajeError = "";
+
+ if(!isset($_POST['id'])) {
+   $hayErrores = true;
+   $mensajeError = "El usuario no existe";
+ }
+ if(($mes>10 && $dia>1)) {
+    $hayErrores = true;
+    $mensajeError = "La fecha de confirmación ya pasó";
+ }
+
+ if($hayErrores) { ?>
+    <p><?php echo $mensajeError;?></p>
+<?php } else {
+    $id = strip_tags($_POST['id']);
+    $sql = 'select * from Usuarios where id ='.$id;
+    $resultado = $conexion->query($sql);
+    $Usuario = mysqli_fetch_object($resultado);
+
+    if($Usuario->total_confirmaciones>=3) {
+        echo 'Llegaste al límite de confirmaciones';
+    } else {
+        $sql="update Usuarios set total_confirmaciones = total_confirmaciones+1 where id =".$Usuario->id;
+        $resultado=$conexion->query($sql);
+        echo "Gracias por confirmar";
+    }
+ }
+ mysqli_close($conexion);
+
    ?>
 
-<?php
-  $sql = "update Usuarios set boletos_confirmados = boletos_asignados where id=".$id;
-  $conexion=mysqli_connect('localhost','root','','susy');
-  $resultado = $conexion->query($sql);
-  
-  echo "<h1>Gracias por confirmar</h1>";
-
-?>
-
-<?php 
- if(!isset($_POST['boletos_confirmados'])) {
-    echo 'ERROR';
-    die();
-  } 
-
-$user_id 						 	= $_POST['id']; 
-$boletos_confirmados  = $_POST['boletos_confirmados']; 
-
-//verificar 1) si aun estas en fecha de confirmar, 2) aun puedes seguir mandando confirmaciones
-  
-?>
-    
 </body>
 </html>
--- invitacion.php
+++ invitacion.php
@@ -1,3 +1,15 @@
+<?php
+//esta línea nos evita mostrar errores tipo warning
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try{
+    //$conexion=mysqli_connect('localhost','root','','susy');
+    $conexion=mysqli_connect('localhost','root','secret','susy');
+
+}catch(Exception $e) {
+    die('No hay conexión a la base de datos');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -10,15 +22,14 @@
     <body>
         <header></header>
         <?php
-        $conexion=mysqli_connect('localhost','root','','susy');
         //print_r($_POST);
-    $hayErrores=false;
-    $mensajeError="";
-    if(!isset($_POST["nombre"])){
-        $hayErrores=true;
-        $mensajeError="No estas registrado";
-        die();
-    }
+        $hayErrores=false;
+        $mensajeError="";
+        if(!isset($_POST["nombre"])){
+            $hayErrores=true;
+            $mensajeError='No estas registrado. <a href="index.php">Regresar</a>';
+            die($mensajeError);
+        }
     //Leemos los datos que vienen en el formulario
     $nombre=strip_tags($_POST["nombre"]);
     $apellido=strip_tags($_POST["apellido"]);
@@ -26,65 +37,51 @@
 
     $sql = 'select * from Usuarios where nombre like "'.$nombre.'%"and apellido like"'.$apellido.'%";';
     $resultado=$conexion->query($sql);
-    
+
     if($resultado->num_rows==0){
         $hayErrores=true;
         $mensajeError="La persona no existe";
-        
+
     }
     if(!$hayErrores){
+        // si no hay errores, buscamos en el resultado que trajo el registro de ese usuario y lo convertimos a objeto
+        // para poder usarlo
         $Usuario = mysqli_fetch_object($resultado);
     }
     ?>
-    <div>
+
     <?php if($hayErrores){ ?>
-          Se encontraron errores: <?php echo $mensajeError;?>
-          <a href="index.php">Da click aqui para intentarlo de nuevo</a>
-    <?php }else{ ?>
- 
-        <header class="heading-primary">
-
-            //tu html de la invitacion
-            <h3 class="heading-primary-main">Bienvenido <?php echo $Usuario->nombre. ' '. $Usuario->apellido?>,</h3>
-            <p class="heading-primary-sub">Te invitamos a nuestros 25 años de Susy y Garo</p>
-            <p class="heading-primary-third">Recuerda que tienes <?php echo $Usuario->boletos_asignados?> boletos asignados</p>
-            Da click  para confirmar tu asistencia
-            <a href="confirmar.php?id=<?php echo $Usuario->id?>">Confirmar asistencia</a>
-        </header>
-       
-   </div>
-    <!--
-
-        verificar la fecha, si estas en fecha, mostrar la confirmacion
-    -->
-  
-    <?php
-    $mes = date('m');
-    $dia = date('d');
-
-    if(($mes>10 && $dia>1) || $Usuario->total_confirmaciones>=3){
-        echo "Lo sentimos ya paso la fecha para confirmar a través de la página, puedes llamarnos para confirmar tu asistencia al número +525513413210";
-
-    }else {
-        
-        
-        $sql="update Usuarios set total_confirmaciones = total_confirmaciones+1 where id =".$Usuario->id;
-        $resultado=$conexion->query($sql);
-        echo "Gracias por confirmar";
+        <div>
+            <header class="heading-primary">
+                Se encontraron errores: <?php echo $mensajeError;?>
+                <a href="index.php">Da click aqui para intentarlo de nuevo</a>
+            </header>
+        </div>
+    <?php } else {
+         $mes = date('m');
+         $dia = date('d');
         ?>
-        <form action="confirmacion.php" method="post">
-           Boletos: <select name="boletos_confirmados">
+
+        <div>
+            <header class="heading-primary">
+                <h3 class="heading-primary-main">Bienvenido <?php echo $Usuario->nombre. ' '. $Usuario->apellido?>,</h3>
+                <p class="heading-primary-sub">Te invitamos a nuestros 25 años de Susy y Garo</p>
+                <p class="heading-primary-third">Recuerda que tienes <b><?php echo $Usuario->boletos_asignados?></b> boletos asignados</p>
+                <form action="confirmacion.php" method="post">
+                    Boletos: <select name="boletos_confirmados">
                        <?php for($i=0;$i<= $Usuario->boletos_asignados; $i++){?>
                            <option value="<?php echo $i?>"><?php echo $i?></option>
                         <?php }?>
                     </select>
                    <input type="hidden" name="id" value="<?php echo $Usuario->id?>">
                    <button type="submit">Confirmar</button>
+                 </form>
+            </header>
+        </div>
+        <?php }?>
 
-             </form>
-    
-    <?php }
-    
+<?php
+ mysqli_close($conexion);
     /*
     if($Usuario->total_confirmaciones<3){
         $sql="update Usuarios set total_confirmaciones = total_confirmaciones+1 where id =".$Usuario->id;
@@ -93,8 +90,8 @@
     }else{
         echo "Ya no puedes confirmar";
     }
-    
-} 
+
+}
 */
 ?>
 
@@ -104,10 +101,10 @@
      if (result.data.todook == true) {
        alert('todo confirmado')
      }
-    })	
+    })
    </script>
-   
-     
-     
+
+
+
 </body>
 </html>
